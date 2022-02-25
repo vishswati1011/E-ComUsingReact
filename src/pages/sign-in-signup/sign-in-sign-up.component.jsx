c790631aa7133d68a5a07629aa5086ffaa921698
vishswat11011 <sati.v1011@gmail.com>
2022-02-25 15:48:19 +0530

    sign in with email using firebase

--- src/pages/sign-in-signup/sign-in-sign-up.component.jsx
+++ src/pages/sign-in-signup/sign-in-sign-up.component.jsx
@@ -2,8 +2,10 @@ import React from "react";
 
 import './sign-in-sign-up.style.scss'
 import SignIn from '../../components/sign-in/sign-in.component'
+import SignUp from "../../components/sign-up/sign-up-component";
 const SignInAndSignUpPage = () =>(
-    <div className="sign-in-and-sign-up"><SignIn/></div>
+    <div className="sign-in-and-sign-up"><SignIn/>
+    <SignUp/></div>
 );
 
 export default SignInAndSignUpPage;
